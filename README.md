5105ca05529b546e553dbe0e2590d5c6913c306a
Shai Haim <shaihaim42@gmail.com>
2024-12-07 15:49:14 +0200

    Running the application using 2 different dockerfiles that sharing the same network

--- README.md
+++ README.md
@@ -18,3 +18,43 @@ vagrant destroy
 # Assuming you are running from the VagrantFile dir:
 vagrant ssh
 ```
+<!-- bazel build //backend:devops_assignment_image -->
+
+## Running the app using 2 docker images from the host:
+1. create docker network for the 2 containers to communicate:
+```bash
+docker network create app-network
+```
+2. create mongodb container:
+```bash
+docker run -d \
+  --name mongodb \
+  --network app-network \
+  -e MONGO_INITDB_ROOT_USERNAME=root \
+  -e MONGO_INITDB_ROOT_PASSWORD=rootpassword \
+  -p 27017:27017 \
+  mongo:latest
+```
+or without password:
+```bash
+docker run -d \
+  --name mongodb \
+  --network app-network \
+  -p 27017:27017 \
+  mongo:latest --noauth
+
+```
+
+3. Build nodejs app:
+```bash
+docker build -t nodeapp .
+```
+
+4. run the app container using the same network:
+```bash
+docker run -d \
+  --name nodeapp:1.0.0 \
+  --network app-network \
+  -p 3000:3000 \
+  nodeapp
+```
--- vm/requirements/install_packages.sh
+++ vm/requirements/install_packages.sh
@@ -4,33 +4,73 @@
 sudo dnf update -y
 
 # Install Git, Vim, and dependencies for Node.js
-sudo dnf install -y git vim
+sudo dnf install -y git vim wget curl gnupg
+
+# Installing docker:
+sudo dnf install -y dnf-utils device-mapper-persistent-data lvm2
+sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo dnf install -y docker-ce docker-ce-cli containerd.io
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker $USER
+newgrp docker
+
+
+# Installing Google Bazel building tool:
+curl -fsSL https://bazel.build/bazel-release.pub.gpg | sudo tee /usr/share/keyrings/bazel-archive-keyring.gpg
+sudo dnf config-manager --add-repo https://storage.googleapis.com/download.bazel.build/rpm/stable.repo
+sudo dnf install -y dnf-plugins-core
+sudo dnf copr enable vbatts/bazel -y
+sudo dnf install -y bazel4
+bazel --version
+
+
+# Installing k3s:
+# Downloading the installation script:
+curl -sfL https://get.k3s.io | sh -
+
+# Validating that k3s is up and running:
+sudo systemctl status k3s
+
+# Configuring non root access to k3s:
+sudo cp /etc/rancher/k3s/k3s.yaml /home/$(whoami)/.kube/config
+sudo chown $(whoami):$(whoami) /home/$(whoami)/.kube/config
+
+k3s --version
+
+# ====================================== In order to run the app directly from the VM - without cluster =================================
 
 # Install Node.js (using the NodeSource repository for the latest version)
-curl -sL https://rpm.nodesource.com/setup_18.x | sudo -E bash -
-sudo dnf install -y nodejs
+# curl -sL https://rpm.nodesource.com/setup_18.x | sudo -E bash -
+# sudo dnf install -y nodejs
 
 
-# Add the MongoDB repository (before installation)
-echo "[mongodb-org-6.0]
-name=MongoDB 6.0 Repository
-baseurl=https://repo.mongodb.org/yum/amazon/2/mongodb-org/6.0/x86_64/
-gpgcheck=1
-enabled=1
-gpgkey=https://www.mongodb.org/static/pgp/server-6.0.asc" | sudo tee /etc/yum.repos.d/mongodb-org-6.0.repo
+# # Add the MongoDB repository (before installation)
+# echo "[mongodb-org-6.0]
+# name=MongoDB 6.0 Repository
+# baseurl=https://repo.mongodb.org/yum/amazon/2/mongodb-org/6.0/x86_64/
+# gpgcheck=1
+# enabled=1
+# gpgkey=https://www.mongodb.org/static/pgp/server-6.0.asc" | sudo tee /etc/yum.repos.d/mongodb-org-6.0.repo
 
-# Install MongoDB
-sudo dnf install -y mongodb-org
+# # Install MongoDB
+# sudo dnf install -y mongodb-org
 
-# Enable and start MongoDB service
-sudo systemctl enable --now mongod
+# # Enable and start MongoDB service
+# sudo systemctl enable --now mongod
 
-# Install npm dependencies:
-npm install ../app/backend --no-bin-links # Symlinks is not working properly between the host and the VM volume.
+# # Install npm dependencies:
+# npm install ../app/backend --no-bin-links # Symlinks is not working properly between the host and the VM volume.
+
+# ====================================== In order to run the app directly from the VM - without cluster =================================
+
 
 # Verify installations
 git --version
 vim --version
-node --version
-npm --version
-mongod --version
+# node --version
+# npm --version
+# mongod --version
+docker --version
+bazel --version
+k3s --version
